8f0a3d77b4198e9befaed7a4e137a0545a3b6d95
KYu3RPI <yuk3@rpi.edu>
2023-04-16 17:04:36 -0400

    Edited to make change View possible Changed some of the code in building, import_schedule, navbar, and start_page to make it easier to change view later.

--- start_page.py
+++ start_page.py
@@ -52,7 +52,31 @@ def new_custom_time(date,hour,minute):
         else:
             return f'{date}, 00:00'
 
+@app.callback(
+    Output("view", "children"),
+    [
+        Input("Student", "n_clicks"),
+        Input("Faculty", "n_clicks"),
+        Input("Staff", "n_clicks"),
+        Input("Guest", "n_clicks"),
+    ],
+)
 
+def change_view(student,faculty,staff,guest):
+    view = 0   # show all buildings
+    if student:
+        map.reloadMap(1)
+        return f"Student View."
+    if faculty:
+        map.reloadMap(2)
+        return f"Faculty View."
+    if staff:
+        map.reloadMap(3)
+        return f"Staff View."
+    if guest:
+        map.reloadMap(4)
+        return f"Guest View."
+    return "" # Button not clicked yet
 
 # Server
 if __name__ == '__main__':
